bb7d7bd18ac133ed254481fa67de7961c39a0eca
Joseph McNeal Jr <TheMofaDe@users.noreply.github.com>
2020-09-09 07:59:11 -0400

    more test coverage

--- tests/DotNetHelper-IO-Tests/IO/Write/WriteAsyncTestFixture.cs
+++ tests/DotNetHelper-IO-Tests/IO/Write/WriteAsyncTestFixture.cs
@@ -11,137 +11,180 @@ using NUnit.Framework;
 
 namespace DotNetHelper.IO.Tests
 {
-	[NonParallelizable]
-	public class WriteTestFixture : BaseTest
-	{
+    [NonParallelizable]
+    public class WriteTestFixture : BaseTest
+    {
 
-		public FolderObject TestFolder { get; }
+        public FolderObject TestFolder { get; }
 
-		public WriteTestFixture()
-		{
-			TestFolder = new FolderObject(WorkingDirectory);
-		}
-
-
-
-		[OneTimeSetUp]
-		public void ClassInit()
-		{
-			// Executes once for the test class. (Optional)
-			new FolderObject(BaseFolder).Delete(true); // PURGE EVERYTHING
-		}
-
-		[OneTimeTearDown]
-		public void ClassCleanup()
-		{
-			// Runs once after all tests in this class are executed. (Optional)
-			// Not guaranteed that it executes instantly after all tests from the class.
-			new FolderObject(BaseFolder).Delete(true); // PURGE EVERYTHING
-		}
-
-		[SetUp]
-		public void TestInit()
-		{
-			// Runs before each test. (Optional)
-		}
-
-		[TearDown]
-		public void TestCleanup()
-		{
-			// Runs after each test. (Optional)
-			TestFolder.Delete(true); // PURGE EVERYTHING
-		}
+        public WriteTestFixture()
+        {
+            TestFolder = new FolderObject(WorkingDirectory);
+        }
 
 
 
-		[Author("Joseph McNeal Jr", "josephmcnealjr@gmail.com")]
-		[Test]
-		public void Test_Write_To_File_Append()
-		{
-			var testFile = RandomTestFileNoExtension;
+        [OneTimeSetUp]
+        public void ClassInit()
+        {
+            // Executes once for the test class. (Optional)
+            new FolderObject(BaseFolder).Delete(true); // PURGE EVERYTHING
+        }
 
-			testFile.Write("A", FileOption.Append);
-			Assert.That(File.ReadAllText(testFile.FullName).Equals("A"));
+        [OneTimeTearDown]
+        public void ClassCleanup()
+        {
+            // Runs once after all tests in this class are executed. (Optional)
+            // Not guaranteed that it executes instantly after all tests from the class.
+            new FolderObject(BaseFolder).Delete(true); // PURGE EVERYTHING
+        }
 
-			testFile.Write("B".ToByteArray(), FileOption.Append);
-			Assert.That(File.ReadAllText(testFile.FullName).Equals("AB"));
+        [SetUp]
+        public void TestInit()
+        {
+            // Runs before each test. (Optional)
+        }
 
-			testFile.Write("C".ToStream(), FileOption.Append);
-			Assert.That(File.ReadAllText(testFile.FullName).Equals("ABC"));
-		}
-
-		[Author("Joseph McNeal Jr", "josephmcnealjr@gmail.com")]
-		[Test]
-		public void Test_Write_To_File_Overwrite()
-		{
-			var testFile = RandomTestFileNoExtension;
-
-			testFile.Write("A", FileOption.Overwrite);
-			Assert.That(File.ReadAllText(testFile.FullName).Equals("A"));
-
-			testFile.Write("B".ToByteArray(), FileOption.Overwrite);
-			Assert.That(File.ReadAllText(testFile.FullName).Equals("B"));
-
-			testFile.Write("C".ToStream(), FileOption.Overwrite);
-			Assert.That(File.ReadAllText(testFile.FullName).Equals("C"));
-		}
-
-
-		[Author("Joseph McNeal Jr", "josephmcnealjr@gmail.com")]
-		[Test]
-		public void Test_Write_To_File_DoNothingIfFileExist()
-		{
-			var testFile = RandomTestFileNoExtension;
-
-			testFile.Write("A", FileOption.DoNothingIfExist);
-			Assert.That(File.ReadAllText(testFile.FullName).Equals("A"));
-
-			testFile.Write("B".ToByteArray(), FileOption.DoNothingIfExist);
-			Assert.That(File.ReadAllText(testFile.FullName).Equals("A"));
-
-			testFile.Write("C".ToStream(), FileOption.DoNothingIfExist);
-			Assert.That(File.ReadAllText(testFile.FullName).Equals("A"));
-		}
-
-		[Author("Joseph McNeal Jr", "josephmcnealjr@gmail.com")]
-		[Test]
-		public void Test_Write_To_File_IncrementFileExtensionIfExist()
-		{
-			var testFile = RandomTestFileNoExtension;
-
-			testFile.Write("A", FileOption.IncrementFileExtensionIfExist);
-			var newFileWithNameIncruemented = testFile.Write("B".ToByteArray(), FileOption.IncrementFileExtensionIfExist);
-			var newFileWithNameIncruemented2 = testFile.Write("C".ToStream(), FileOption.IncrementFileExtensionIfExist);
-
-			Assert.That(File.ReadAllText(testFile.FullName).Equals("A"));
-			Assert.That(File.ReadAllText(newFileWithNameIncruemented).Equals("B"));
-			Assert.That(File.ReadAllText(newFileWithNameIncruemented2).Equals("C"));
-
-			Assert.AreNotSame(newFileWithNameIncruemented2, newFileWithNameIncruemented);
-			Assert.AreNotSame(testFile, newFileWithNameIncruemented);
-		}
-
-
-		[Author("Joseph McNeal Jr", "josephmcnealjr@gmail.com")]
-		[Test]
-		public void Test_Write_To_File_IncrementFileNameIfExist()
-		{
-			var testFile = RandomTestFileNoExtension;
-
-			testFile.Write("A", FileOption.IncrementFileNameIfExist);
-			var newFileWithNameIncruemented = testFile.Write("B".ToByteArray(), FileOption.IncrementFileNameIfExist);
-			var newFileWithNameIncruemented2 = testFile.Write("C".ToStream(), FileOption.IncrementFileNameIfExist);
-
-			Assert.That(File.ReadAllText(testFile.FullName).Equals("A"));
-			Assert.That(File.ReadAllText(newFileWithNameIncruemented).Equals("B"));
-			Assert.That(File.ReadAllText(newFileWithNameIncruemented2).Equals("C"));
-
-			Assert.AreNotSame(newFileWithNameIncruemented2, newFileWithNameIncruemented);
-			Assert.AreNotSame(testFile, newFileWithNameIncruemented);
-		}
+        [TearDown]
+        public void TestCleanup()
+        {
+            // Runs after each test. (Optional)
+            TestFolder.Delete(true); // PURGE EVERYTHING
+        }
 
 
 
+        [Author("Joseph McNeal Jr", "josephmcnealjr@gmail.com")]
+        [Test]
+        public void Test_Write_To_File_Append()
+        {
+            var testFile = RandomTestFileNoExtension;
+
+            testFile.Write("A", FileOption.Append);
+            Assert.That(File.ReadAllText(testFile.FullName).Equals("A"));
+
+            testFile.Write("B".ToByteArray(), FileOption.Append);
+            Assert.That(File.ReadAllText(testFile.FullName).Equals("AB"));
+
+            testFile.Write("C".ToStream(), FileOption.Append);
+            Assert.That(File.ReadAllText(testFile.FullName).Equals("ABC"));
+        }
+
+        [Author("Joseph McNeal Jr", "josephmcnealjr@gmail.com")]
+        [Test]
+        public void Test_Write_To_File_Overwrite()
+        {
+            var testFile = RandomTestFileNoExtension;
+
+            testFile.Write("A", FileOption.Overwrite);
+            Assert.That(File.ReadAllText(testFile.FullName).Equals("A"));
+
+            testFile.Write("B".ToByteArray(), FileOption.Overwrite);
+            Assert.That(File.ReadAllText(testFile.FullName).Equals("B"));
+
+            testFile.Write("C".ToStream(), FileOption.Overwrite);
+            Assert.That(File.ReadAllText(testFile.FullName).Equals("C"));
+        }
+
+
+        [Author("Joseph McNeal Jr", "josephmcnealjr@gmail.com")]
+        [Test]
+        public void Test_Write_To_File_DoNothingIfFileExist()
+        {
+            var testFile = RandomTestFileNoExtension;
+
+            testFile.Write("A", FileOption.DoNothingIfExist);
+            Assert.That(File.ReadAllText(testFile.FullName).Equals("A"));
+
+            testFile.Write("B".ToByteArray(), FileOption.DoNothingIfExist);
+            Assert.That(File.ReadAllText(testFile.FullName).Equals("A"));
+
+            testFile.Write("C".ToStream(), FileOption.DoNothingIfExist);
+            Assert.That(File.ReadAllText(testFile.FullName).Equals("A"));
+        }
+
+        [Author("Joseph McNeal Jr", "josephmcnealjr@gmail.com")]
+        [Test]
+        public void Test_Write_To_File_IncrementFileExtensionIfExist()
+        {
+            var testFile = RandomTestFileNoExtension;
+
+            testFile.Write("A", FileOption.IncrementFileExtensionIfExist);
+            var newFileWithNameIncruemented = testFile.Write("B".ToByteArray(), FileOption.IncrementFileExtensionIfExist);
+            var newFileWithNameIncruemented2 = testFile.Write("C".ToStream(), FileOption.IncrementFileExtensionIfExist);
+
+            Assert.That(File.ReadAllText(testFile.FullName).Equals("A"));
+            Assert.That(File.ReadAllText(newFileWithNameIncruemented).Equals("B"));
+            Assert.That(File.ReadAllText(newFileWithNameIncruemented2).Equals("C"));
+
+            Assert.AreNotSame(newFileWithNameIncruemented2, newFileWithNameIncruemented);
+            Assert.AreNotSame(testFile, newFileWithNameIncruemented);
+        }
+
+
+        [Author("Joseph McNeal Jr", "josephmcnealjr@gmail.com")]
+        [Test]
+        public void Test_Write_To_File_IncrementFileNameIfExist()
+        {
+            var testFile = RandomTestFileNoExtension;
+
+            testFile.Write("A", FileOption.IncrementFileNameIfExist);
+            var newFileWithNameIncruemented = testFile.Write("B".ToByteArray(), FileOption.IncrementFileNameIfExist);
+            var newFileWithNameIncruemented2 = testFile.Write("C".ToStream(), FileOption.IncrementFileNameIfExist);
+
+            Assert.That(File.ReadAllText(testFile.FullName).Equals("A"));
+            Assert.That(File.ReadAllText(newFileWithNameIncruemented).Equals("B"));
+            Assert.That(File.ReadAllText(newFileWithNameIncruemented2).Equals("C"));
+
+            Assert.AreNotSame(newFileWithNameIncruemented2, newFileWithNameIncruemented);
+            Assert.AreNotSame(testFile, newFileWithNameIncruemented);
+        }
+
+
+
+        [Author("Joseph McNeal Jr", "josephmcnealjr@gmail.com")]
+        [Test]
+        public void Test_Write_To_File_IncrementFileNameIfExistWithNumberExtension()
+        {
+            var testFile = new FileObject(RandomTestFileWithExtension.FileNameOnly + "1.1");
+
+            testFile.Write("A", FileOption.IncrementFileNameIfExist);
+            var newFileWithNameIncruemented = testFile.Write("B".ToByteArray(), FileOption.IncrementFileNameIfExist);
+            var newFileWithNameIncruemented2 = testFile.Write("C".ToStream(), FileOption.IncrementFileNameIfExist);
+
+            Assert.That(File.ReadAllText(testFile.FullName).Equals("A"));
+            Assert.That(File.ReadAllText(newFileWithNameIncruemented).Equals("B"));
+            Assert.That(File.ReadAllText(newFileWithNameIncruemented2).Equals("C"));
+
+            Assert.AreNotSame(newFileWithNameIncruemented2, newFileWithNameIncruemented);
+            Assert.AreNotSame(testFile, newFileWithNameIncruemented);
+
+            // Assert.That(newFileWithNameIncruemented.Equals(testFile.FileNameOnly.Replace("1.", "2.") + ".1"));
+            // Assert.That(newFileWithNameIncruemented2.Equals(testFile.FileNameOnly.Replace("2.", "3.") + ".1"));
+        }
+
+
+
+        [Author("Joseph McNeal Jr", "josephmcnealjr@gmail.com")]
+        [Test]
+        public void Test_Write_To_File_IncrementFileExtensionIfExist_WithNumberExtension()
+        {
+            var testFile = new FileObject(RandomTestFileNoExtension.FullName + "1.1");
+
+            testFile.Write("A", FileOption.IncrementFileExtensionIfExist);
+            var newFileWithNameIncruemented = testFile.Write("B".ToByteArray(), FileOption.IncrementFileExtensionIfExist);
+            var newFileWithNameIncruemented2 = testFile.Write("C".ToStream(), FileOption.IncrementFileExtensionIfExist);
+
+            Assert.That(File.ReadAllText(testFile.FullName).Equals("A"));
+            Assert.That(File.ReadAllText(newFileWithNameIncruemented).Equals("B"));
+            Assert.That(File.ReadAllText(newFileWithNameIncruemented2).Equals("C"));
+
+            Assert.AreNotSame(newFileWithNameIncruemented2, newFileWithNameIncruemented);
+            Assert.AreNotSame(testFile, newFileWithNameIncruemented);
+
+            // Assert.That(newFileWithNameIncruemented.Equals(testFile.FilePathOnly + testFile.FileNameOnly + ".2"),$"wrong name {newFileWithNameIncruemented} vs {testFile.FullName}");
+            // Assert.That(newFileWithNameIncruemented2.Equals(testFile.FilePathOnly + testFile.FileNameOnly + ".3"),$"wrong name {newFileWithNameIncruemented2} vs {testFile.FullName}");
+        }
 
 
 
@@ -150,8 +193,5 @@ namespace DotNetHelper.IO.Tests
 
 
 
-
-
-
-	}
+    }
 }
